845fce004549a2a44d2bff842c2070ad3e9c5412
clem <mail@clemens-cords.com>
2023-01-15 19:28:56 +0100

    done

--- scrape.py
+++ scrape.py
@@ -3,12 +3,14 @@ import asyncio
 import sys
 import os
 import csv
+import requests
 from datetime import datetime
 
+import douyin_tiktok_scraper.scraper
 from douyin_tiktok_scraper.scraper import Scraper
 
 scraper = Scraper()
-async def download_metadata(url: str) -> dict:
+async def download_video_data(url: str) -> dict:
     """
     download video metadata
 
@@ -17,14 +19,20 @@ async def download_metadata(url: str) -> dict:
     """
     return await scraper.hybrid_parsing(video_url=url)
 
-async def download_video(id):
+async def get_video_download_link(id: str) -> str:
     """
-    download video file
+    get direct download link for video
 
     :param url: link to video in the form https://www.douyin.com/video/<video id>
-    :return:
+    :return: dictionary with metadata
     """
-    print("todo")
+
+    # super hacky, the download interface doesn't seem to be exposed so if this link ever changes
+    # this will break. Is there maybe a resource link somewhere in video data?
+
+    download_url_left = "https://api.douyin.wtf/download?url=https%3A%2F%2Fwww.douyin.com%2Fvideo%2F"
+    download_url_right = "&prefix=true&watermark=false"
+    return download_url_left + id + download_url_right
 
 async def video_url_to_video_id(url: str) -> str:
     """
@@ -63,32 +71,42 @@ async def main():
     out_dir = "./out"
     warn_if_target_dir_is_file(out_dir)
 
-    print("Initializing session output at " + os.path.abspath(out_dir))
+    print("[LOG] Initializing session output at " + os.path.abspath(out_dir))
 
     if not os.path.isdir(out_dir):
         os.mkdir("./out")
 
     video_dicts = []
 
-    csv_file = open(out_dir + "/out_" + timestamp.strftime("%d_%m_%Y_%H_%M_%S") + ".csv", "w")
+    csv_file = open(out_dir + "/out_" + timestamp.strftime("%d_%m_%H_%M_%S") + ".csv", "w")
     csv_writer = csv.DictWriter(csv_file, [])
     csv_writer_initialized = False
 
-    # download and format metadata
+    # download and format metadata and video
     for video_url in video_urls:
 
         video_id = await video_url_to_video_id(video_url)
         video_dir = out_dir + "/" + video_id
 
+        print("\n[LOG] Downloading video data for video " + video_id + "...")
+
         warn_if_target_dir_is_file(video_dir)
         if not os.path.isdir(video_dir):
             os.mkdir(video_dir)
 
-        metadata = await download_metadata(video_url)
+        json_raw_path = video_dir + "/raw.json"
+        metadata = await download_video_data(video_url)
         metadata["request_timestamp"] = datetime.timestamp(datetime.now())
-        json_raw_file = open(video_dir + "/raw.json", "w")
+        json_raw_file = open(json_raw_path, "w")
         json_raw_file.write(json.dumps(metadata))
         json_raw_file.close()
+        print("[LOG] Wrote metadata to " + os.path.abspath(json_raw_path))
+
+        video_path = video_dir + "/video.mp4"
+        download_link = await get_video_download_link(video_id)
+        req = requests.get(download_link, allow_redirects=True)
+        open(video_path, 'wb').write(req.content)
+        print("[LOG] Wrote video to" + os.path.abspath(video_path))
 
         # extract values
         def extract_keys_recursively(dict_in, items):
@@ -110,10 +128,9 @@ async def main():
             csv_writer_initialized = True
 
         csv_writer.writerow(items)
-        print("\nWrote video data to " + items["local_uri"] + "\n")
 
     csv_file.close()
-    print("Wrote csv output for session to " + os.path.abspath(out_dir + "/out.csv"))
+    print("[LOG] Wrote csv output for session to " + os.path.abspath(out_dir + "/out.csv"))
     print("done.")
 
 asyncio.run(main())
